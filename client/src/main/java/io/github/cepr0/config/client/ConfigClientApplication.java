061c62519613c8a8e14f73df6aea8cdc0799873b
Sergei Poznanski <cepr0@ukr.net>
2020-08-31 15:09:22 +0300

    Implement config refreshing by remote event

--- client/src/main/java/io/github/cepr0/config/client/ConfigClientApplication.java
+++ client/src/main/java/io/github/cepr0/config/client/ConfigClientApplication.java
@@ -1,11 +1,15 @@
 package io.github.cepr0.config.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @SpringBootApplication
 @EnableConfigurationProperties(DemoProps.class)
@@ -24,5 +28,10 @@ public class ConfigClientApplication {
     @GetMapping("/demo")
     public DemoProps demo() {
         return props;
+    }
+
+    @EventListener
+    public void handleRefreshRemoteApplicationEvent(RefreshRemoteApplicationEvent event) {
+        log.info("[i] Received an event: {}", event);
     }
 }
--- server/src/main/java/io/github/cepr0/config/server/ConfigController.java
+++ server/src/main/java/io/github/cepr0/config/server/ConfigController.java
@@ -1,8 +1,10 @@
 package io.github.cepr0.config.server;
 
 import lombok.Value;
+import org.springframework.cloud.bus.BusProperties;
 import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,32 +14,40 @@ import java.util.List;
 
 @RestController
 @RequestMapping("properties")
-public class ConfigController {
+public class ConfigController implements ApplicationEventPublisherAware {
 
+    public static final String DESTINATION_SERVICE = "demo-client";
+
+    private ApplicationEventPublisher publisher;
+    
     private final PropertyRepo propertyRepo;
-    private final ApplicationEventPublisher publisher;
+    private final BusProperties busProperties;
 
-    public ConfigController(
-            PropertyRepo propertyRepo,
-            ApplicationEventPublisher publisher
-    ) {
+    public ConfigController(PropertyRepo propertyRepo, BusProperties busProperties) {
         this.propertyRepo = propertyRepo;
-        this.publisher = publisher;
+        this.busProperties = busProperties;
     }
 
     @PutMapping("/demo")
     public List<Property> demo(@Valid @RequestBody DemoConfig demoConfig) {
         List<Property> properties = propertyRepo.saveAll(List.of(
-                new Property("demo-client", "demo.text", demoConfig.text),
-                new Property("demo-client", "demo.num", demoConfig.num)
+                new Property(DESTINATION_SERVICE, "demo.text", demoConfig.text),
+                new Property(DESTINATION_SERVICE, "demo.num", demoConfig.num)
         ));
-        publisher.publishEvent(new RefreshRemoteApplicationEvent(this, "config-service", "demo-client"));
+
+        String busId = busProperties.getId();
+        publisher.publishEvent(new RefreshRemoteApplicationEvent(this, busId, DESTINATION_SERVICE));
         return properties;
     }
 
     @GetMapping
     public List<Property> getAll() {
         return propertyRepo.findAll();
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
     }
 
     @Value
